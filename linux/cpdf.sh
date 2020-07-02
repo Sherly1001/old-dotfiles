786132f2f40111ea34c040acb21905f912c50b55
Sherly1001 <Sherly1001@users.noreply.github.com>
2020-07-02 20:19:57 +0700

    add shell copy file

---
+++ linux/cpdf.sh
@@ -0,0 +1,32 @@
+
+if [[ -z $1 ]]; then
+    echo "usage: ./cpdf.sh [opt]"
+    echo "[opt]: -a: copy all files"
+    echo "       -b: copy bash files"
+    echo "       -v: copy vim files"
+    echo "       -z: copy zsh files"
+fi
+
+if [[ $(echo $@ | grep -e '\-b' -e '-a') ]]; then
+    cp .inputrc ~/
+    echo "cp .inputrc ~/"
+    cp .bashrc ~/
+    echo "cp .bashrc ~/"
+    cp .sher.sh ~/
+    echo "cp .sher.sh ~/"
+    cp .git-prompt.sh ~/
+    echo "cp .git-prompt.sh ~/"
+fi
+
+if [[ $(echo $@ | grep -e '\-v' -e '-a') ]]; then
+    cp -fr .vim ~/
+    echo "cp -fr .vim ~/"
+    cp .vimrc ~/
+    echo "cp .vimrc ~/"
+fi
+
+if [[ $(echo $@ | grep -e '\-z' -e '-a') ]]; then
+    cp .zshrc ~/
+    echo "cp .zshrc ~/"
+fi
+
