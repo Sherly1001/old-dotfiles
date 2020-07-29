40ca5fc6a839672e4b30d2470dd22e843c513185
Sherly1001 <Sherly1001@users.noreply.github.com>
2020-07-30 00:32:55 +0700

    add build function and clang-format function

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -22,10 +22,52 @@ function fm($file) {
     Invoke-Expression "astyle -A2s4SpHUk3W3xfxhOn $file"
 }
 
+function clfm() {
+    foreach ($file in $args) {
+        Invoke-Expression "clang-format --style='{ AllowShortFunctionsOnASingleLine: Empty, AllowShortIfStatementsOnASingleLine: WithoutElse, AllowShortLoopsOnASingleLine: true, IncludeBlocks: Regroup, IndentCaseLabels: true, IndentWidth: 4, SortIncludes: false }' -i $file"
+    }
+}
+
+function b {
+    param([switch]$g)
+
+    if (-not $args[0]) {
+        Write-Host -fore red "No input file"
+        return
+    }
+
+    $out = [System.IO.Path]::GetFileNameWithoutExtension($args[0]) + '.exe'
+    $ext = [System.IO.Path]::GetExtension($args[0])
+
+    $cc = 'clang '
+    if ($ext -like '.cpp') {
+        $cc = 'clang++ '
+    }
+    if ($g) {
+        $cc += '-g '
+    }
+
+    if ($args[1] -and $args[1] -ne '.') {
+        $out = $args[1]
+        if ([System.IO.Path]::GetExtension($out) -eq '') {
+            $out += '.exe'
+        }
+    }
+
+    $cc += "'$($args[0])' "
+    $cc += "-o '$($out)'"
+
+    foreach($i in $args[2..$args.length]) {
+        $cc += " '$i'"
+    }
+
+    echo "$cc"
+}
+
 Remove-Item Alias:cls
 function cls {
 Clear-Host
-echo "                   _____    __                       __            ___   ____     ____     ___
+write-host -fore green "                   _____    __                       __            ___   ____     ____     ___
                   / ___/   / /_     ___     _____   / /  __  __   <  /  / __ \   / __ \   <  /
                   \__ \   / __ \   / _ \   / ___/  / /  / / / /   / /  / / / /  / / / /   / / 
                  ___/ /  / / / /  /  __/  / /     / /  / /_/ /   / /  / /_/ /  / /_/ /   / /  
@@ -33,7 +75,7 @@ echo "                   _____    __                       __            ___   _
                                                       /____/                                  "
 }
 
-echo "                   _____    __                       __            ___   ____     ____     ___
+write-host -fore green "                   _____    __                       __            ___   ____     ____     ___
                   / ___/   / /_     ___     _____   / /  __  __   <  /  / __ \   / __ \   <  /
                   \__ \   / __ \   / _ \   / ___/  / /  / / / /   / /  / / / /  / / / /   / / 
                  ___/ /  / / / /  /  __/  / /     / /  / /_/ /   / /  / /_/ /  / /_/ /   / /  
