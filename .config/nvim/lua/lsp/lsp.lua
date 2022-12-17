b93063f2ead607b1a0238f3ccd295638a45d7e10
Sherly1001 <longlepkd2000@gmail.com>
2022-12-18 01:50:29 +0700

    nvim: ts formatter

--- .config/nvim/lua/lsp/lsp.lua
+++ .config/nvim/lua/lsp/lsp.lua
@@ -1,11 +1,48 @@
 -- vi: ts=2 sw=2
 
+local prettier_fmt = {
+  formatCommand = 'prettier --stdin-filepath ${INPUT}',
+  formatStdin = true,
+}
+
+local langs = {
+  typescript = { prettier_fmt },
+  javascript = { prettier_fmt },
+  typescriptreact = { prettier_fmt },
+  javascriptreact = { prettier_fmt },
+  ["typescript.tsx"] = { prettier_fmt },
+  ["javascript.jsx"] = { prettier_fmt },
+  vue = { prettier_fmt },
+  yaml = { prettier_fmt },
+  json = { prettier_fmt },
+  html = { prettier_fmt },
+  scss = { prettier_fmt },
+  css = { prettier_fmt },
+  markdown = { prettier_fmt },
+}
+
 local lsp = {
   bashls = {},
-  tsserver = {},
+  tsserver = {
+    on_attach = function(client)
+      local rc = client.server_capabilities
+      -- vim.api.nvim_echo({{vim.inspect(rc)}}, true, {})
+      rc.documentFormattingProvider = false
+      rc.documentRangeFormattingProvider = false
+    end,
+  },
   clangd = {},
   gopls = {},
   rust_analyzer = {},
+  efm = {
+    filetypes = vim.tbl_keys(langs),
+    init_options = {
+      documentFormatting = true,
+    },
+    settings = {
+      languages = langs,
+    },
+  },
   sumneko_lua = {
     settings = {
       Lua = {
